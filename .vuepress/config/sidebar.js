32a9bab1996f8a60a2fcd64dbc8431f6513d7966
Mondo <568197478@qq.com>
2020-03-11 16:57:26 +0800

    fix: 修改执行上下文

--- .vuepress/config/sidebar.js
+++ .vuepress/config/sidebar.js
@@ -2,19 +2,19 @@
 
   const NodeJS = [["01.sql语句增减删查.md","sql语句增减删查"]]
 
-  const Notes = [["01.公司面试系列.md","公司面试系列"],["02.利用js-xlsx前端导出Excel.md","利用js-xlsx前端导出Excel"],["03.前端常见下载处理方案.md","前端常见下载处理方案"],["04.2019年总结.md","2019年总结"]]
+  const Notes = [["00.VueReact小程序.md","VueReact小程序"],["01.公司面试系列.md","公司面试系列"],["02.利用js-xlsx前端导出Excel.md","利用js-xlsx前端导出Excel"],["03.前端常见下载处理方案.md","前端常见下载处理方案"],["04.2019年总结.md","2019年总结"]]
 
-  const ReactJS = [["01.React基础.md","React基础"],["02.TypeScript与react结合一些注意点.md","TypeScript与react结合一些注意点"]]
+  const ReactJS = [["00.React入门.md","React入门"],["01.React路由.md","React路由"],["02.React状态管理.md","React状态管理"],["03.TypeScript与react结合一些注意点.md","TypeScript与react结合一些注意点"]]
 
   const TypeScript = [["01.基础类型与联合类型.md","基础类型与联合类型"]]
 
   const VueJS = [["01.组件通信处理方案.md","组件通信处理方案"],["02.基于饿了么组件封装.md","基于饿了么组件封装"],["03.后台管理项目总结.md","后台管理项目总结"],["04.使用过程小记.md","使用过程小记"],["05.项目nginx部署.md","项目nginx部署"],["06.组件编码规范.md","组件编码规范"],["07.简单的一个Store.md","简单的一个Store"]]
 
-  const webpack = [["00.webpack基础.md","webpack基础"],["01.项目打包优化方案.md","项目打包优化方案"]]
+  const webpack = [["00.webpack基础.md","webpack基础"],["01.项目打包优化方案.md","项目打包优化方案"],["02.webpack多页应用打包.md","webpack多页应用打包"],["03.webpack与ESLint 结合.md","webpack与ESLint 结合"],["04.webpack打包库和组件.md","webpack打包库和组件"],["05.webpack实现SSR打包.md","webpack实现SSR打包"],["06.优化命令行和构建捕获.md","优化命令行和构建捕获"],["07.构建配置包设计.md","构建配置包设计"],["08.loader开发.md","loader开发"],["09.webpack插件的开发.md","webpack插件的开发"]]
 
-  const 前端知识 = [{"title":"JavaScript","collapsable":false,"children":[["01.JavaScript/01.基础数据.md","基础数据"],["01.JavaScript/02.函数.md","函数"],["01.JavaScript/03.原型链.md","原型链"],["01.JavaScript/04.执行上下文.md","执行上下文"],["01.JavaScript/05.作用域和作用域链.md","作用域和作用域链"],["01.JavaScript/06.闭包.md","闭包"],["01.JavaScript/07.创建对象模式.md","创建对象模式"],["01.JavaScript/08.JavaScript线程机制与事件机制.md","JavaScript线程机制与事件机制"],["01.JavaScript/09.异步编程promise.md","异步编程promise"],["01.JavaScript/10.排序算法.md","排序算法"]]},{"title":"CSS","collapsable":false,"children":[["02.CSS/01.box-sizing，你的宽高度计算对了吗.md","box-sizing，你的宽高度计算对了吗"],["02.CSS/02.预编译.md","预编译"]]}]
+  const 前端知识 = [{"title":"JavaScript","collapsable":false,"children":[["01.JavaScript/01.基础数据.md","基础数据"],["01.JavaScript/02.函数.md","函数"],["01.JavaScript/03.原型链.md","原型链"],["01.JavaScript/04.执行上下文.md","执行上下文"],["01.JavaScript/05.作用域和作用域链.md","作用域和作用域链"],["01.JavaScript/06.闭包.md","闭包"],["01.JavaScript/07.创建对象模式.md","创建对象模式"],["01.JavaScript/08.JavaScript线程机制与事件机制.md","JavaScript线程机制与事件机制"],["01.JavaScript/09.异步编程promise.md","异步编程promise"],["01.JavaScript/10.排序算法.md","排序算法"],["01.JavaScript/11.防抖与节流.md","防抖与节流"]]},{"title":"CSS","collapsable":false,"children":[["02.CSS/00.垂直居中布局.md","垂直居中布局"],["02.CSS/01.box-sizing，你的宽高度计算对了吗.md","box-sizing，你的宽高度计算对了吗"],["02.CSS/02.预编译.md","预编译"]]}]
 
-  const 小程序 = [["01.小程序项目入坑实践.md","小程序项目入坑实践"],["02.小程序的初次遇见,使用mpvue搭建模板.md","小程序的初次遇见,使用mpvue搭建模板"]]
+  const 小程序 = [["01.小程序项目入坑实践.md","小程序项目入坑实践"],["02.小程序的初次遇见,使用mpvue搭建模板.md","小程序的初次遇见,使用mpvue搭建模板"],["03.小程序订阅消息.md","小程序订阅消息"]]
 
   const 工程化 = [["01.cmder.md","cmder"],["02.git.md","git"],["03.npm.md","npm"],["04.gulp.md","gulp"],["05.eslintrc.md","eslintrc"],["06.vscode同步.md","vscode同步"],["07.xshell命令.md","xshell命令"],["08.Github项目实现自动化部署.md","Github项目实现自动化部署"],["09.Nuxt项目从开始到部署.md","Nuxt项目从开始到部署"],["10.vuepress与travis-cli持续集成自动化部署.md","vuepress与travis-cli持续集成自动化部署"],["11.Docker.md","Docker"]]
 
