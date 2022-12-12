ce3ae07f1c47fad94886e0725b8eeb414ac197ea
imondo <mondo@imondo.cn>
2022-12-12 05:29:57 +0000

    Deploying to gh-pages from @ imondo/docs@a4f0420fac28523c55f18902983988def2a82d6a 🚀

---
+++ assets/guide_02_JS探索_06_闭包.md.a8515f72.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as n,o as a,a as l}from"./app.90b3aaf2.js";const C=JSON.parse('{"title":"闭包","description":"","frontmatter":{},"headers":[],"relativePath":"guide/02_JS探索/06_闭包.md","lastUpdated":1670822938000}'),p={name:"guide/02_JS探索/06_闭包.md"},o=l("",23),e=[o];function c(t,r,y,F,D,A){return a(),n("div",null,e)}const d=s(p,[["render",c]]);export{C as __pageData,d as default};
